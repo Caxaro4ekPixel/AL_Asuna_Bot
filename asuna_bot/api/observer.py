24c671e0f440e80a8e552171e349e2c584e90247
im4xxx <korochkin20000@gmail.com>
2023-09-24 03:46:46 +1300

    переименован файл и сам класс, перенес декораторы сокета в класс Observer, использован синглтон для этого класса

--- asuna_bot/api/observer.py
+++ asuna_bot/api/observer.py
@@ -5,36 +5,52 @@ from loguru import logger as log
 
 from asuna_bot.db.mongo import Mongo as db
 from asuna_bot.db.odm.bot_conf import NyaaRssConf
-from asuna_bot.main.chat_control import ChatControl
-
-from .model import NyaaTorrent
+from asuna_bot.main.chat_controller import ChatController
+from asuna_bot.db.odm import Chat
+from asuna_bot.api.models import NyaaTorrent
 from .rss_parser import rss_to_json
 
+from anilibria import TitleEpisode
+from asuna_bot.main.anilibria_client import al_client
 
-class NyaaRssFeed:
+class WsRssObserver:
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        
+        return cls.__instance 
+
     def __init__(self) -> None:
-        self._chats = dict()
+        self.chats = dict()
         self._session = ClientSession()
         self._running : bool = True
         self._config : NyaaRssConf
 
+        # Это декораторы websocket event
+        al_client.on(TitleEpisode)(self._ws_episode_update)
+
     async def _register_chats(self):
         all_ongoings = await db.get_all_ongoing_chats()
         for chat in all_ongoings:
-            if chat.id not in self._chats.keys():
+            chat: Chat
+            if chat.id not in self.chats.keys():
                 await self._register_chat(chat)
-                
-        
+                        
     async def _register_chat(self, chat):
-        new_chat = ChatControl(chat)
-        self._chats[chat.id] = new_chat
+        self.chats[chat.id] = ChatController(chat)
 
-
-    async def _push_update(self, torrents) -> None:
-        for chat in self._chats.values():
+    async def _push_rss_update(self, torrents) -> None:
+        for chat in self.chats.values():
+            chat: ChatController
             await chat.nyaa_update(torrents)
+        
+    async def _ws_episode_update(self, event: TitleEpisode) -> None:
+        for chat in self.chats.values():
+            chat: ChatController
+            await chat.episode_update(event)
             
-
     async def _rss_request(self, url: str, params: dict, limit):
         try:
             response = await self._session.get(url, params=params, timeout=30)
@@ -50,7 +66,6 @@ class NyaaRssFeed:
             log.debug(ex)
         
         return json
-
 
     async def start_polling(self):
         """Поллинг rss ленты"""
@@ -78,11 +93,10 @@ class NyaaRssFeed:
                     for torrent in parsed_rss 
                     if torrent.get("id") > conf.last_id
                 ]
-                await self._push_update(torrents) # Делаем пуш чатам
+                await self._push_rss_update(torrents) # Делаем пуш чатам
                 await db.update_nyaa_rss_conf(last_id=rss_last_id)
 
             await asyncio.sleep(conf.interval)
-
 
     def _build_params_str(self) -> None:
         if self._config.submitters:
