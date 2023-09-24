b03f36764a9c5d356fbf2ddb76a3770b8c063ea5
im4xxx <korochkin20000@gmail.com>
2023-09-24 16:48:58 +1300

    добавлена функция изменения равки

--- asuna_bot/handlers/__init__.py
+++ asuna_bot/handlers/__init__.py
@@ -3,5 +3,13 @@ from .ass_to_srt import convert_router
 from .registration import admin_router
 from .start import start_router
 from .supergroup import supergroup_router
+from .submitters import submitter_router
 
-__routers__ = (dev_router, convert_router, admin_router, start_router, supergroup_router)
+__routers__ = (
+    dev_router, 
+    convert_router, 
+    admin_router, 
+    start_router, 
+    supergroup_router,
+    submitter_router,
+)
